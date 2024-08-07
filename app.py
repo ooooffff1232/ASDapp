9d53f631a896c6f9b3b1edcc84e0581a7596bf03
SavarToteja <savartot@gmail.com>
2024-08-07 06:17:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -241,5 +241,5 @@ input_data = {
 if st.button("Get Predictions"):
     prediction = get_prediction(input_data)
     st.subheader(f"Education Type: {prediction}")
-    if st.button("What does that mean?"):
+    st.subheader(prediction)
       
